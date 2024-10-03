699040486dd82c426aa49c00b61adbbb37aed4b6
Carin Barber <cbarber2289@gmail.com>
2024-10-03 14:38:23 -0500

    Checking

--- site.js
+++ site.js
@@ -1,4 +1,16 @@
-const contact = document.querySelector('#contact')
-contact.addEventListener('mouseover', () => {
-    contact.closest('nav').style.backgroundColor = 'lightblue'
-})
+const hours = new Date().getHours() // get the current hour
+const welcomeD = document.querySelector('#welcome')
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if(isMorning) {
+    welcomeD.textContent = 'Good Morning'
+}
+    else if(isAfternoon) {
+        welcomeD.textContent = 'Good Afternoon'
+    }
+    else {
+        welcomeD.textContent = 'Good Evening'
+    }
